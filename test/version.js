24bf9b06dc056e7185f02915aeae0bf400e8295c
Stefanie Holbrook <stefholbrook@gmail.com>
2016-12-09 10:28:58 -0700

    Assignment 3 completed

--- test/version.js
+++ test/version.js
@@ -1,9 +1,25 @@
 const Code = require('code')
 const Lab = require('lab')
 
-const lab = exports.
+const Package = require('../package.json')
+const University = require('../lib')
+
+const lab = exports.lab = Lab.script()
+const describe = lab.experiment
 const expect = Code.expect
+const it = lab.test
 
 describe('/version', () => {
+  it('returns the version from package.json', (done) => {
+    University.init(0, (err, server) => {
+      expect(err).to.not.exist()
 
+      server.inject('/version', (res) => {
+        expect(res.statuscode).to.equal(200)
+        expect(res.result).to.equal({ version: Package.version })
+
+        server.stop(done)
+      })
+    })
+  })
 })
