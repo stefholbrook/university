e6eed36ca292b11d772d74c8897b47f1a94f081e
Stefanie Holbrook <stefholbrook@gmail.com>
2016-12-10 14:55:58 -0700

    Assignment 6

---
+++ assets/scripts/login.js
@@ -0,0 +1,8 @@
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    document.getElementById('btnLogin').addEventListener('click', function (event) {
+      event.preventDefault()
+      console.log('Login Button Cilcked')
+    })
+  }
+}
